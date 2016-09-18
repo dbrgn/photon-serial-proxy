dfa97686129cfa381bec5e6dedea194a08fd48cd
Danilo Bargen <mail@dbrgn.ch>
2016-09-19 01:09:06 +0200

    Serial proxy

--- main.ino
+++ main.ino
@@ -1,12 +1,26 @@
 void setup() {
     Serial.begin(9600);
-    Serial.setTimeout(1000);
+    Serial1.begin(9600);
+    Serial.setTimeout(300);
+    Serial1.setTimeout(300);
+}
+
+String processLine(String line) {
+    if (line.equals("v")) {
+        return String("EBB Arduino Clone v0.1.0");
+    }
+    return NULL;
 }
 
 void loop() {
-    String line = Serial.readStringUntil('\r'); // Start of \r\n
+    String line = Serial.readStringUntil('\n');
     if (line != NULL) {
-        Serial.read(); // Remove \n from buffer
-        Particle.publish("serialLine", line);
+        // Pass on serial data
+        Serial1.write("> " + line + "\n");
+        String back = processLine(line.substring(0, line.length() - 1));
+        if (back != NULL) {
+            Serial.write(back + "\r\n");
+            Serial1.write("< " + back + "\r\n");
+        }
     }
 }
